b168c738910331115338a7364f0fad19b67fc896
Aditya Mishra <emailadi77@gmail.com>
2026-07-08 19:55:17 +0530

    Practice

---
+++ Patterns/rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n;
+    cout << "Enter the number of columns : ";
+    cin >> m;
+    cout << "Enter the number of row : ";
+    cin >> n;
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
