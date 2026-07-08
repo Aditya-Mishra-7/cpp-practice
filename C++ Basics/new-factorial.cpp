19158f3bee7d7ef870a3730fd1f1a7916a1e99ad
Aditya Mishra <emailadi77@gmail.com>
2026-07-08 10:01:40 +0530

    Practice

---
+++ C++ Basics/new-factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, Fact = 1;
+    cout << "Enter the value for n : ";
+    cin >> n;
+    if (n >= 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Fact *= i;
+            cout << i <<" Factorial is : ";
+            cout << Fact << endl ;
+        }
+        
+    }
+    else
+    {
+        cout << "Invalid input!";
+    }
+}
